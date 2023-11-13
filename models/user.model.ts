c329b56f8388a09e5fdccec89c6d160f83a3ce60
shubham.prateek <sprateek2338@gmail.com>
2023-11-13 21:26:47 +0530

    Adding access tokens

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,6 +1,7 @@
+require("dotenv").config();
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
-
+import jwt from "jsonwebtoken";
 const emailRegexPattern: RegExp =
   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -16,6 +17,8 @@ export interface IUser extends Document {
   isVerified: boolean;
   courses: Array<{ courseId: string }>;
   comparePassword: (password: string) => Promise<boolean>;
+  SignAccessToken: ()=> string;
+  SignRefreshToken: ()=> string;
 }
 
 const userSchema: Schema<IUser> = new mongoose.Schema(
